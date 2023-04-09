76a26a5dc4dc6ef1f2338e569181d77c16524e64
Alexis <63512507+Aldbg74@users.noreply.github.com>
2023-04-09 10:58:56 +0200

    Update
    Update add new code line, still unfonctional

--- bot.js
+++ bot.js
@@ -1,21 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-intents: [
-    Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_MESSAGES
+const { Client, GatewayIntentBits } = require('discord.js')
+const client = new Client({
+  intents: [
+      GatewayIntentBits.Guilds,
   ]
+})
 
-const prefix = '!';
-
-client.on('message', message => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-  
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (command === 'hello') {
-    message.reply('Bonjour mon ami, comment vas-tu aujourd\'hui ?');
-  }
-});
-
-client.login('MTA5NDE2MjY5MzQ5MTAwNzUwOQ.G8TmNs.mpkacSeUc4DdJos24DbMGL7xStQEONau0pwmok');
+client.on ("Message"), message => {
+if (message.content === "ping") {
+message.reply ("pong");
+}
+client.login ("MTA5NDE2MjY5MzQ5MTAwNzUwOQ.GGq8TQ.j9tOKjECcl-6HZt5gbTgmOSkb32A51VBsjwDjw");
+};
